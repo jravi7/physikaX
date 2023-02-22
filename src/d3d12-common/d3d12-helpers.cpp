341d746a6095c8c4cac23bd7b04436358e81a458
Jay Ravi <jaykumarark@gmail.com>
2023-02-21 22:03:22 -0800

    Update d3d12 helpers

--- src/d3d12-common/d3d12-helpers.cpp
+++ src/d3d12-common/d3d12-helpers.cpp
@@ -3,10 +3,11 @@
 #include <comdef.h>  // _com_error
 #include <d3dx12.h>
 
+#include "d3d12-common/d3d12-common.h"
 #include "logger/logger.h"
 
 namespace physika {
-namespace d3d12_util {
+namespace d3d12_common {
 
 char const* HRErrorDescription(HRESULT hr)
 {
@@ -21,5 +22,55 @@ void ThrowIfFailed(HRESULT hr)
     }
 }
 
-}  // namespace d3d12_util
+ID3D12ResourcePtr CreateBuffer(ID3D12DevicePtr pDevice, ID3D12GraphicsCommandListPtr pCmdList,
+                               D3D12_HEAP_PROPERTIES const& heapProperties, uint64_t const size)
+{
+    ID3D12ResourcePtr pBuffer = nullptr;
+
+    if (!pDevice || !pCmdList || size == 0) {
+        return pBuffer;
+    }
+    auto const& bufferDesc = CD3DX12_RESOURCE_DESC::Buffer(size);
+    ThrowIfFailed(pDevice->CreateCommittedResource(&heapProperties, D3D12_HEAP_FLAG_NONE,
+                                                   &bufferDesc, D3D12_RESOURCE_STATE_COMMON,
+                                                   nullptr, IID_PPV_ARGS(pBuffer.GetAddressOf())));
+    return pBuffer;
+}
+
+DefaultGPUBuffer CreateDefaultBuffer(ID3D12DevicePtr pDevice, ID3D12GraphicsCommandListPtr pCmdList,
+                                     void const* initialData, uint64_t const byteSize)
+{
+    if (!pDevice || !pCmdList || !initialData || byteSize == 0) {
+        return { nullptr, nullptr };
+    }
+    auto const& defaultHeapProperties = CD3DX12_HEAP_PROPERTIES(D3D12_HEAP_TYPE_DEFAULT);
+    auto const& uploadHeapProperties  = CD3DX12_HEAP_PROPERTIES(D3D12_HEAP_TYPE_UPLOAD);
+
+    ID3D12ResourcePtr gpuBuffer = CreateBuffer(pDevice, pCmdList, defaultHeapProperties, byteSize);
+    ID3D12ResourcePtr uploadBuffer =
+        CreateBuffer(pDevice, pCmdList, uploadHeapProperties, byteSize);
+
+    // Subresource Data desc
+    D3D12_SUBRESOURCE_DATA subresourceData;
+    subresourceData.pData      = initialData;
+    subresourceData.RowPitch   = byteSize;
+    subresourceData.SlicePitch = byteSize;
+
+    auto const& barrier = CD3DX12_RESOURCE_BARRIER::Transition(
+        gpuBuffer.Get(), D3D12_RESOURCE_STATE_COMMON, D3D12_RESOURCE_STATE_COPY_DEST);
+    pCmdList->ResourceBarrier(1, &barrier);
+
+    // Issue copy
+    UpdateSubresources<1>(pCmdList.Get(), gpuBuffer.Get(), uploadBuffer.Get(), 0, 0, 1,
+                          &subresourceData);
+
+    // Transition to generic read state
+    auto const& barrier2 = CD3DX12_RESOURCE_BARRIER::Transition(
+        gpuBuffer.Get(), D3D12_RESOURCE_STATE_COPY_DEST, D3D12_RESOURCE_STATE_GENERIC_READ);
+    pCmdList->ResourceBarrier(1, &barrier2);
+
+    return { gpuBuffer, uploadBuffer };
+}
+
+}  // namespace d3d12_common
 }  // namespace physika
--- src/d3d12-common/include/d3d12-common/d3d12-helpers.h
+++ src/d3d12-common/include/d3d12-common/d3d12-helpers.h
@@ -2,8 +2,15 @@
 #include <inttypes.h>    // uint64_t
 #include <wrl/client.h>  // ComPtr
 
+#include <tuple>
+
+#include "d3d12-common/d3d12-common.h"
+
 namespace physika {
 namespace d3d12_common {
+
+//! @brief Alias representing a tuple of gpubuffer and its upload heap buffer
+using DefaultGPUBuffer = std::tuple<ID3D12ResourcePtr, ID3D12ResourcePtr>;
 
 //! @brief Returns a string representation of a HRESULT param.
 char const* HRErrorDescription(HRESULT hr);
@@ -11,7 +18,48 @@ char const* HRErrorDescription(HRESULT hr);
 //! @brief throw an exception on HRESULT failure
 void ThrowIfFailed(HRESULT hr);
 
+//! @brief Creates a default GPU Buffer and Upload Heap buffer with initial data.
+//! @out
+ID3D12ResourcePtr CreateBuffer(ID3D12DevicePtr pDevice, ID3D12GraphicsCommandListPtr pCmdList,
+                               D3D12_HEAP_PROPERTIES const& heapProperties, uint64_t const size);
 
+//! @brief Creates a default GPU Buffer and Upload Heap buffer with initial data.
+//! @out
+DefaultGPUBuffer CreateDefaultBuffer(ID3D12DevicePtr pDevice, ID3D12GraphicsCommandListPtr pCmdlist,
+                                     void const* initialData, uint64_t const byteSize);
+
+struct Mesh
+{
+    ID3D12ResourcePtr vertexBufferGPU;
+    ID3D12ResourcePtr vertexBufferUploadHeap;
+    ID3D12ResourcePtr indexBufferGPU;
+    ID3D12ResourcePtr indexBufferUploadHeap;
+
+    DXGI_FORMAT indexFormat         = DXGI_FORMAT_R32_UINT;
+    UINT        indexBufferByteSize = 0;
+    uint32_t    vertexByteStride;
+    uint32_t    vertexBufferByteSize;
+
+    D3D12_VERTEX_BUFFER_VIEW VertexBufferView() const
+    {
+        D3D12_VERTEX_BUFFER_VIEW vbv;
+        vbv.BufferLocation = vertexBufferGPU->GetGPUVirtualAddress();
+        vbv.StrideInBytes  = vertexByteStride;
+        vbv.SizeInBytes    = vertexBufferByteSize;
+
+        return vbv;
+    }
+
+    D3D12_INDEX_BUFFER_VIEW IndexBufferView() const
+    {
+        D3D12_INDEX_BUFFER_VIEW ibv;
+        ibv.BufferLocation = indexBufferGPU->GetGPUVirtualAddress();
+        ibv.Format         = indexFormat;
+        ibv.SizeInBytes    = indexBufferByteSize;
+
+        return ibv;
+    }
+};
 
 }  // namespace d3d12_common
 }  // namespace physika
