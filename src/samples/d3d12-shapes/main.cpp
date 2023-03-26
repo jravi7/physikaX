7f6cd3bdf2fda626594ab53dfc86271cd25da59b
Jay Ravi <jaykumarark@gmail.com>
2023-03-26 11:31:53 -0700

    Add new shapes project

---
+++ src/samples/d3d12-shapes/main.cpp
@@ -0,0 +1,22 @@
+
+#include <stdio.h>
+
+#include "d3d12-shapes.h"
+#include "logger/logger.h"
+
+int main()
+{
+    sample::D3D12Basic app(_T("D3D12 Basic"), 1920, 1080);
+
+    if (!app.Initialize()) {
+        printf("Could not initialize app. Exiting");
+        return 1;
+    }
+    app.Run();
+
+    if (!app.Shutdown()) {
+        printf("Shutdown sequence failed.");
+        return 1;
+    }
+    return 0;
+}
